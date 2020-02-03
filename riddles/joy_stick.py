aa9d8ee2c5d4685bf3d62ede62e2626b7a72e459
Jonny Park <jonnythebard9@gmail.com>
2020-02-03 23:21:10 +0900

    refactore joy stick

--- riddles/joy_stick.py
+++ riddles/joy_stick.py
@@ -1,39 +1,37 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42860/
-name = "JARED"
+name = "JARRED"
+
+from string import ascii_uppercase
+
+l = len(ascii_uppercase)
+a = ascii_uppercase
 
 
 def solution(name):
-    count = 0
-    alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    d = {}
-    indexes = []
-    current_idx = 0
     n = len(name)
+    indice = []
+    cost = 0
 
-    # map vertical costs
-    for i in range(len(alpha)):
-        d[alpha[i]] = min(i, 26 - i)
+    for i, c in enumerate(name):
+        c_idx = a.index(c)
+        if c_idx == 0:
+            continue
+        cost += min(c_idx, l - c_idx)
+        indice.append(i)
 
-    # calculate vertical costs
-    for i in range(n):
-        num = d[name[i]]
-        count += num
-        if num != 0:
-            indexes.append(i)
-
-    # calculate horizontal costs
+    current_idx = 0
     while True:
-        if len(indexes) == 0:
+        if len(indice) == 0:
             break
-        min_dist = 99
+        min_cost = n
         min_idx = 0
-        for idx in indexes:
-            dist = min(abs(idx - current_idx), n - abs(idx - current_idx))
-            if dist < min_dist:
-                min_dist = dist
+        for idx in indice:
+            _cost = min(abs(current_idx - idx), n - abs(current_idx - idx))
+            if _cost < min_cost:
+                min_cost = _cost
                 min_idx = idx
-        count += min_dist
-        indexes.remove(min_idx)
+        cost += min_cost
+        indice.remove(min_idx)
         current_idx = min_idx
 
-    return count
+    return cost
